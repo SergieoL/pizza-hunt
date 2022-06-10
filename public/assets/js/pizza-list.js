7f858b56470bbd00cdcae5bd63af8995c41a5b01
Sergieo <sergieol23@gmail.com>
2022-06-10 01:22:55 -0500

    added comment replies model and routes

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -8,7 +8,8 @@ const getPizzaList = () => {
     })
     .catch(err => {
       console.log(err);
-    })
+    });
+};
 
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
   const pizzaCard = `
@@ -35,8 +36,6 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
   `;
 
   $pizzaList.innerHTML += pizzaCard;
-
-  }
 };
 
 getPizzaList();
