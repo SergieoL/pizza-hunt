c629260d4485b9f8ff6eef091686a04eac807b5e
Sergieo <sergieol23@gmail.com>
2022-06-14 23:11:38 -0500

    added validators

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,10 +4,14 @@ const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: 'You need to proive a pizza name fool!',
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: 'Who created this??',
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
